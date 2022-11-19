c05fd15a909b452093475877070a93d7231f3e56
unknown <udayj4597@gmail.com>
2022-11-19 08:31:43 +0530

    Spring IOC Mobile File

---
+++ SpringIOCApplication/src/main/java/com/Mobile.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Mobile {
+
+	public Mobile() {
+		System.out.println("I have Andriod Mobile Phone");
+	}
+	
+	public void ios() {
+		System.out.println("Iphone");
+	}
+
+}
